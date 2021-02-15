fb8f85db246353fe11d91acfbfa38aabf2c137ce
WellingtonFreitas <wellington.m.defreitas@gmail.com>
2021-02-15 18:15:54 -0300

    Creating connetion with MongoDB

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import { AppProps } from 'next/app';
+import React from 'react';
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NextPage } from 'next';
+
+const IndexPage: NextPage = () => {
+  return (
+    <div>
+      <div className="py-60">
+        <h1 className="text-5lx text-center text-accent-1">
+          Next.js + Tailwind CSS
+        </h1>
+
+      </div>
+    </div>
+  );
+
+}
+
+export default IndexPage;
