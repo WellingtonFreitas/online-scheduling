119a518094f85523926a0e3da5bc0e7bba0afff1
WellingtonFreitas <wellington.m.defreitas@gmail.com>
2021-02-20 23:59:33 -0300

    creating route POST/GET

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import { Provider } from 'next-auth/client';
 import { AppProps } from 'next/app';
 import React from 'react';
+import Head from 'next/head';
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <Provider session={pageProps.session}>
+      <Head>
+        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet" />
+
+      </Head>
       <Component {...pageProps} />
     </Provider>
   );
--- pages/api/user.tsx
+++ pages/api/user.tsx
@@ -10,27 +10,72 @@ interface SucessResponseType {
     name: string;
     email: string
     cellphone: string;
-    custommer: string;
+    barberman: boolean;
+    address: string;
+    services: string[];
+    schedule: Record<string, number[]>;
+    reviews: Record<string, unknown>[];
+    appointments: Record<string, unknown>[];
 }
 
 export default async (
     req: NextApiRequest,
-    res: NextApiResponse<ErrorResponseType | SucessResponseType>
+    res: NextApiResponse
 ): Promise<void> => {
     if (req.method === 'POST') {
-        const { name, email, cellphone, custommer } = req.body;
-        if (!name || !email || !cellphone || !custommer) {
-            res.status(400).json({ error: 'Missing body parameter' });
-            return;
+        const { name, email, cellphone, barberman, address, services, schedule, appointments } = req.body;
+
+        if (barberman) {
+            if (!name || !email || !cellphone || !barberman || !address || !services || !schedule) {
+                res.status(400).json({ error: 'Missing body parameter' });
+                return
+            }
+            
+            try {
+            const { db } = await connect();
+            const response = await db.collection('users').insertOne({
+                name, email, cellphone, barberman, address, services, schedule, appointments,
+            })
+                res.status(201).json(response.ops[0]);
+            } catch (err) {
+                res.status(400).json({ error: 'User create error - ' + err });
+            }
+        }
+        else {
+            if (!name || !email || !cellphone) {
+                res.status(400).json({ error: 'Missing body parameter' });
+                return
+            }
+            try {
+                const { db } = await connect();
+                const response = await db.collection('users').insertOne({
+                    name, email, cellphone, appointments,
+                })
+                res.status(201).json(response.ops[0]);
+            } catch (err) {
+                res.status(400).json({ error: 'User create error - ' + err });
+            }
+        }
+    } else if (req.method === 'GET') {
+
+        const { name } = req.body;
+        console.log('chegou no req bofy')
+        const { db } = await connect();
+
+        if (!name) {
+            console.log('chegou aqui')
+            const users = db.collection('users').find({ 'name': name });
+            res.status(201).json(users);
+        } else {
+            const users = db.collection('users').find();
+            res.status(201).json(users);
         }
 
-        const { db } = await connect();
-        const response = await db.collection('users').insertOne({
-            name, email, cellphone, custommer,
-        })
-        res.status(200).json(response.ops[0]);
-    } else {
-        res.status(400).json({ error: 'Is not post' });
+    }
+
+
+    else {
+        res.status(400).json({ error: 'Wrong request method' });
     }
 
 };
